926ed917445afc8fa04b5449cf61aabf6077afef
wilboron <williammingardi@gmail.com>
2020-09-03 10:16:00 -0300

    Adds the user create endpoint

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -12,4 +12,20 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(self.response.body)
     assert_equal @user.email, json_response['email']
   end
+
+  test 'should create user' do
+    assert_difference('User.count') do
+      user_payload = { user: { email: 'test@test.org', password: '123456' } }
+      post api_v1_users_url, params: user_payload, as: :json
+    end
+    assert_response :created
+  end
+
+  test 'should not create user with taken email' do
+    assert_no_difference('User.count') do
+      user_payload = { user: { email: @user.email, password: '123456' } }
+      post api_v1_users_url, params: user_payload, as: :json
+    end
+    assert_response :unprocessable_entity
+  end
 end
