068dba1a4e4c5cbe4469c473a202ccb8f3d197a5
wilboron <williammingardi@gmail.com>
2020-09-03 10:03:20 -0300

    Add user controller, implement show with tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,17 +2,21 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test 'user with a valid email should be valid' do
-    user = User.new(email: 'test@test.org', password_digest: 'test')
+    user = User.new(email: 'test@test.org', password: 'test')
     assert user.valid?
   end
   test 'user with a invalid email should be invalid' do
-    user = User.new(email: 'test', password_digest: 'test')
+    user = User.new(email: 'test', password: 'test')
     assert_not user.valid?
   end
   test 'user with taken email should be invalid' do
     other_user = users(:one)
-    user = User.new(email: other_user.email, password_digest:
+    user = User.new(email: other_user.email, password:
     'test')
     assert_not user.valid?
-    end
+  end
+  test 'user with empty password should be invalid' do
+    user = User.new(email: users(:one).email)
+    assert_not user.valid?
+  end
 end
