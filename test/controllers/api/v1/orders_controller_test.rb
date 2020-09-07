67d65a2b6ceb5198b801bcce6946661154fab4c3
wilboron <williammingardi@gmail.com>
2020-09-07 18:40:19 -0300

    Adds quantity to placements

--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -9,8 +9,8 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
                           { product_id: products(:one).id, quantity: 2 },
                           { product_id: products(:two).id, quantity: 3 }
                         ]
-    }
-    } }
+                       }
+                    }
   end
 
   test 'should forbid orders for unlogged' do
