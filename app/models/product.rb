2ef05a6fba700b7cb959a50c50d315b620fcd467
wilboron <williammingardi@gmail.com>
2020-09-06 21:03:14 -0300

    Adds some validations to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
+  validates :title, presence: true
+  validates :user, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
   belongs_to :user
 end
