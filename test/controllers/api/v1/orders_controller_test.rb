544c8632fb8b95c56614410dbdd8e46b38bf50a9
wilboron <williammingardi@gmail.com>
2020-09-07 17:32:47 -0300

    Adds the show action for order

--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -19,4 +19,15 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(response.body)
     assert_equal @order.user.orders.count, json_response['data'].count
   end
+
+  test 'should show order' do
+    get api_v1_order_url(@order),
+        headers: { Authorization: JsonWebToken.encode(user_id: @order.user_id) },
+        as: :json
+    assert_response :success
+
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    title = json_response.dig(:included, 0, :attributes, :title)
+    assert_equal @order.products.first.title, title
+  end
 end
